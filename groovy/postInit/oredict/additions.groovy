26515dcc3a87592b790843debee4fb2ae0bc7d3c
ursamina <143211829+ursamina@users.noreply.github.com>
2024-03-31 16:24:10 +0100

    oredict the bop dirt

--- groovy/postInit/oredict/additions.groovy
+++ groovy/postInit/oredict/additions.groovy
@@ -1,7 +1,3 @@
-
-// Gregtech
-ore('itemSilicon').add(item('gregtech:meta_plate', 99)) // GT Silicon Plate
-
 // AE2
 ore('gemFluix').add(item('appliedenergistics2:material', 7))
 
@@ -10,14 +6,32 @@ ore('gemZanite').add(item('aether:zanite_gemstone'))
 ore('gemIcestone').add(item('aether:icestone'))
 ore('gemAmbrosium').add(item('aether:ambrosium_shard'))
 
-// Midnight
-ore('gemEbonite').add(item('midnight:ebonys'))
+// Biomes o' Plenty
+ore('dirtLoamy').add(item('biomesoplenty:dirt'))
+ore('dirtLoamy').add(item('biomesoplenty:dirt', 8))
+ore('dirt').add(item('biomesoplenty:dirt', 8))
+
+ore('dirtSandy').add(item('biomesoplenty:dirt', 1))
+ore('dirtSandy').add(item('biomesoplenty:dirt', 9))
+ore('dirt').add(item('biomesoplenty:dirt', 1))
+ore('dirt').add(item('biomesoplenty:dirt', 9))
+
+ore('dirtSilty').add(item('biomesoplenty:dirt', 2))
+ore('dirtSilty').add(item('biomesoplenty:dirt', 10))
+ore('dirt').add(item('biomesoplenty:dirt', 2))
+ore('dirt').add(item('biomesoplenty:dirt', 10))
 
 // Botania
 ore('gemDragonstone').add(item('botania:manaresource', 9))
 ore('blockManasteel').add(item('botania:storage'))
 ore('blockTerrasteel').add(item('botania:storage', 1))
 ore('blockElvenElementium').add(item('botania:storage', 2))
+
+// Midnight
+ore('gemEbonite').add(item('midnight:ebonys'))
+
+// Gregtech
+ore('itemSilicon').add(item('gregtech:meta_plate', 99)) // GT Silicon Plate
 
 // Thaumic Bases
 ore('ingotThauminite').add(item('thaumicbases:thauminite_ingot'))

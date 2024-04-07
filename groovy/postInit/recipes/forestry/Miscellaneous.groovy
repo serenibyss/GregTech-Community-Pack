b81ff0501f79437488a8b7cc177d072ebebaa18e
ursamina <143211829+ursamina@users.noreply.github.com>
2024-04-07 17:32:19 +0100

    add misc forestry recipes
    add smoker recipe (not that it gets used but it's still there)
    add pipette recipe requiring glass bolt generation

--- groovy/postInit/recipes/forestry/Miscellaneous.groovy
+++ groovy/postInit/recipes/forestry/Miscellaneous.groovy
@@ -27,3 +27,29 @@ crafting.shapedBuilder()
         .key("C", item("minecraft:minecart"))
         .key("s", ore("toolScrewdriver"))
         .register()
+
+
+crafting.removeByOutput(item('forestry:smoker'))
+crafting.shapedBuilder()
+        .name('forestry/misc/bee_smoker')
+        .output(item('forestry:smoker'))
+        .matrix('LTs',
+                'LFh',
+                'TTT')
+        .key("h", ore("toolHammer"))
+        .key("T", ore("plateTin"))
+        .key("s", ore("toolScrewdriver"))
+        .key("F", item("minecraft:flint_and_steel"))
+        .key("L", item("minecraft:leather"))
+        .register()
+
+crafting.removeByOutput(item('forestry:pipette'))
+crafting.shapedBuilder()
+        .name('forestry/misc/pipette')
+        .output(item('forestry:pipette'))
+        .matrix('  R',
+                ' G ',
+                'G  ')
+        .key("R", ore("plateRubber"))
+        .key("G", ore("boltGlass"))
+        .register()
